68c37f264f473f5c10bd6f11903d76d58f78295d
TomDo1234 <tomtheawesome123@gmail.com>
2023-08-16 13:30:41 +1000

    proper indentation 2

--- app/Main.hs
+++ app/Main.hs
@@ -4,28 +4,28 @@ main :: IO ()
 q1 sum current_num = 
     if current_num < 1000
         then 
-            if mod current_num 3 == 0 || mod current_num 5 == 0
-                then q1 (sum + current_num) (current_num + 1)
-                else q1 sum (current_num + 1)
-        else sum
+        if mod current_num 3 == 0 || mod current_num 5 == 0
+            then q1 (sum + current_num) (current_num + 1)
+        else q1 sum (current_num + 1)
+    else sum
 
 q2 sum prev_num current_num = 
     if current_num < 4000000
         then 
-            if mod current_num 2 == 0
-                then q2 (sum + current_num) current_num (current_num + prev_num)
-                else q2 sum current_num (current_num + prev_num)
-        else sum
+        if mod current_num 2 == 0
+            then q2 (sum + current_num) current_num (current_num + prev_num)
+        else q2 sum current_num (current_num + prev_num)
+    else sum
 
 
 
 q3 checked_number current_divisor  = 
     if current_divisor >= checked_number
         then checked_number
-        else 
-            if mod checked_number current_divisor == 0
-                then q3 (div checked_number current_divisor) (current_divisor)
-                else q3 checked_number (current_divisor + 1)
+    else 
+        if mod checked_number current_divisor == 0
+            then q3 (div checked_number current_divisor) (current_divisor)
+        else q3 checked_number (current_divisor + 1)
 
 check_palindrome_number number = 
     let numberAsString = show number
